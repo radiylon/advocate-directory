0bc016f6ac642bcf3fb926063f4f6edd705e285a
radiylon <radiylon@gmail.com>
2026-01-22 22:51:34 -0800

    Self-host Lato font with hinted WOFF2 files for Windows rendering

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Suspense } from "react";
-import { Lato } from "next/font/google";
 import localFont from "next/font/local";
 import { Analytics } from "@vercel/analytics/next";
 import { Providers } from "./components/Providers";
@@ -8,10 +7,19 @@ import { AppDisclaimer } from "./components/AppDisclaimer";
 import { Footer } from "./components/Footer";
 import "./globals.css";
 
-const lato = Lato({
-  subsets: ["latin"],
-  weight: ["100", "300", "400", "700", "900"],
-  style: ["normal", "italic"],
+const lato = localFont({
+  src: [
+    { path: "../fonts/lato/lato-100.woff2", weight: "100", style: "normal" },
+    { path: "../fonts/lato/lato-100-italic.woff2", weight: "100", style: "italic" },
+    { path: "../fonts/lato/lato-300.woff2", weight: "300", style: "normal" },
+    { path: "../fonts/lato/lato-300-italic.woff2", weight: "300", style: "italic" },
+    { path: "../fonts/lato/lato-400.woff2", weight: "400", style: "normal" },
+    { path: "../fonts/lato/lato-400-italic.woff2", weight: "400", style: "italic" },
+    { path: "../fonts/lato/lato-700.woff2", weight: "700", style: "normal" },
+    { path: "../fonts/lato/lato-700-italic.woff2", weight: "700", style: "italic" },
+    { path: "../fonts/lato/lato-900.woff2", weight: "900", style: "normal" },
+    { path: "../fonts/lato/lato-900-italic.woff2", weight: "900", style: "italic" },
+  ],
   variable: "--font-lato",
 });
 
